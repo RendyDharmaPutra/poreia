a7683b08079c5842c4fc9a1fdc555f1dd42e83b4
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-04 17:32:37 +0700

    feat: add placeholder at select courier & change certain styling component on searchbar component

--- src/components/layout/header/Searchbar.vue
+++ src/components/layout/header/Searchbar.vue
@@ -16,14 +16,15 @@ const onSubmit = () => {
   >
     <USelect
       size="xl"
-      class="w-48"
+      placeholder="Pilih Ekspedisi"
       :items="LIST_COURIERS"
       v-model="selectedCourier"
     />
-    <UInput size="xl" class="w-2/3" v-model="awb" />
+    <UInput size="xl" class="w-full" v-model="awb" />
     <UButton
       label="Lacak Sekarang"
-      class="flex-1 px-8 h-12 text-lg text-inverted"
+      size="xl"
+      class="flex-1 px-8 text-lg text-inverted"
       @click="onSubmit"
     />
   </section>
