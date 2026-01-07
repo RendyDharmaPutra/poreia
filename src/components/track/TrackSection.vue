b42a67c01818669e761bcce486ed47598c1820f5
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-07 14:11:36 +0700

    feat: add track history component

--- src/components/track/TrackSection.vue
+++ src/components/track/TrackSection.vue
@@ -1,8 +1,42 @@
+<script setup lang="ts">
+const histories = [
+  {
+    date: "2026-01-07 09:35:05",
+    desc: "[Cikarang Barat DC] Paket telah diterima Pusat Sortir",
+  },
+  {
+    date: "2026-01-07 03:56:40",
+    desc: "Parcel [TO20260107TKPMT] transporting to [Cikarang Barat DC]",
+  },
+  {
+    date: "2026-01-07 01:43:50",
+    desc: "[Transit Point Karawaci DC] Paket telah diterima Pusat Sortir",
+  },
+  {
+    date: "2026-01-06 22:55:05",
+    desc: "Parcel [TO20260106SZT2V] transporting to [Transit Point Karawaci DC]",
+  },
+  {
+    date: "2026-01-06 21:47:50",
+    desc: "[Cikupa 2 First Mile Hub] Paket telah diterima Pick-up Hub",
+  },
+  {
+    date: "2026-01-06 20:50:56",
+    desc: "[Cikupa 2 First Mile Hub] Paket anda telah diambil dari penjual",
+  },
+  {
+    date: "2026-01-06 12:03:10",
+    desc: "Nomor Resi Pengiriman telah dibuat",
+  },
+];
+</script>
+
 <template>
   <UContainer
     as="section"
     class="px-4 py-12 grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-4xl"
   >
     <TrackSummary />
+    <TrackHistory :histories="histories" />
   </UContainer>
 </template>
---
+++ src/components/track/history/TrackHistory.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useDateFormat } from "@vueuse/core";
+
+const props = defineProps<{
+  histories: {
+    date: string;
+    desc: string;
+  }[];
+}>();
+</script>
+>
+
+<template>
+  <section class="md:col-span-2 flex flex-col space-y-8">
+    <div class="flex flex-row items-center space-x-2">
+      <UIcon name="i-lucide-clock" class="size-6" />
+      <p class="font-bold text-xl md:text-2xl">Riwayat Perjalanan</p>
+    </div>
+
+    <div>
+      <UTimeline
+        :items="props.histories"
+        :default-value="0"
+        color="primary"
+        class="w-full max-w-full"
+      >
+        <template #title="{ item }">
+          <span>{{ item.desc }}</span>
+        </template>
+
+        <template #date="{ item }">
+          {{
+            useDateFormat(item.date, "DD MMM YYYY HH:mm", {
+              locales: "id-ID",
+            })
+          }}
+        </template>
+      </UTimeline>
+    </div>
+  </section>
+</template>
