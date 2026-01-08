6494b8690644731695b79fa6fa1fab53e6c079af
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-08 18:06:37 +0700

    ref: remove logging

--- src/stores/track.store.ts
+++ src/stores/track.store.ts
@@ -18,26 +18,18 @@ export const useTrackStore = defineStore("track", () => {
   const state = reactive(initialState());
 
   const load = async (courier: string, awb: string) => {
-    console.log("track"); // ? Debug
     reset();
 
     state.status = "loading";
-    console.log(`isLoading: ${state.status === "loading"}`);
 
     const res = await trackPackageService(courier, awb);
-    console.log(res);
     if (res.status === 200) {
       state.data = res.data!;
     } else {
       state.error = res.message;
     }
 
-    // ? Debug
-    console.log(state.data);
-    console.log(state.error);
-
     state.status = "success";
-    console.log(`isLoading: ${state.status === "loading"}`);
   };
 
   const reset = () => Object.assign(state, initialState());
