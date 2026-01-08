6daecbf3128f8184dee6fefe23742633ec4845ea
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-09 01:11:52 +0700

    feat: showing page content based on track store state

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useTrackStore } from "../stores/track.store";
+
+const trackStore = useTrackStore();
+
+const isIdle = computed(() => trackStore.state.status === "idle");
+const hasError = computed(() => trackStore.state.status === "error");
+const isReady = computed(() => trackStore.state.status === "success");
+</script>
 
 <template>
   <UContainer
     as="section"
     class="px-4 py-12 flex justify-center items-center max-w-4xl w-full min-h-[400px]"
   >
-    <!-- <EmptyState /> -->
-    <TrackSection />
-    <!-- <ErrorState /> -->
+    <EmptyState v-if="isIdle" />
+    <ErrorState v-else-if="hasError" />
+    <TrackSection v-else-if="isReady" />
   </UContainer>
 </template>
